4ff883a2c034a9cc2c83ee7ee63312ab0584593a
Richiey1 <ojodamilarey@gmail.com>
2025-12-25 16:26:40 +0100

    fix: change network to mainnet to match contract address

--- frontend/config/constants.ts
+++ frontend/config/constants.ts
@@ -1,5 +1,5 @@
 // Contract configuration
-export const STACKS_NETWORK = 'testnet'; // 'mainnet' or 'testnet'
+export const STACKS_NETWORK = 'mainnet'; // 'mainnet' or 'testnet'
 export const CONTRACT_ADDRESS = 'SP258BY8D71JCTV73A4V3ADPHCVWSBEM6G4FETPYF';
 export const CONTRACT_NAME = 'stackstactoe-fix';
 export const CONTRACT_FULL_NAME = `${CONTRACT_ADDRESS}.${CONTRACT_NAME}`;
