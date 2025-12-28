05ae9725690c300d043f2f89ddbe8119b7ba46e3
Richiey1 <ojodamilarey@gmail.com>
2025-12-28 18:55:21 +0100

    Update contract name to stackstactoe-alpha
    - Changed CONTRACT_NAME from stackstactoe-beta to stackstactoe-alpha
    - Points frontend to newly deployed contract with win detection fixes
    - Contract address: SP258BY8D71JCTV73A4V3ADPHCVWSBEM6G4FETPYF.stackstactoe-alpha

--- frontend/config/constants.ts
+++ frontend/config/constants.ts
@@ -1,7 +1,7 @@
 // Contract configuration
 export const STACKS_NETWORK = 'mainnet'; // 'mainnet' or 'testnet'
 export const CONTRACT_ADDRESS = 'SP258BY8D71JCTV73A4V3ADPHCVWSBEM6G4FETPYF';
-export const CONTRACT_NAME = 'stackstactoe-beta';
+export const CONTRACT_NAME = 'stackstactoe-alpha';
 export const CONTRACT_FULL_NAME = `${CONTRACT_ADDRESS}.${CONTRACT_NAME}`;
 
 export const APP_NAME = 'StacksTacToe';
