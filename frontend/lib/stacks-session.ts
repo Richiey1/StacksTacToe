75f72a7fc5a98e275b19e12866a302d3d1c595fc
Richiey1 <ojodamilarey@gmail.com>
2026-05-30 00:05:25 +0100

    Cleanup: update app name in stacks session details

--- frontend/lib/stacks-session.ts
+++ frontend/lib/stacks-session.ts
@@ -5,6 +5,6 @@ const appConfig = new AppConfig(["store_write", "publish_data"]);
 export const userSession = new UserSession({ appConfig });
 
 export const appDetails = {
-  name: "Stacks Multisig Vaults",
+  name: "StacksTacToe",
   icon: "/favicon.ico",
 };
